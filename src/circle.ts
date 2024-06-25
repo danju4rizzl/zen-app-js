ce2a1f54783a0afcf3eff0431129792ea834d406
danju4rizzl <danjumashiwaju@gmail.com>
2024-06-25 10:19:32 +0200

    minor updates

---
+++ src/circle.ts
@@ -0,0 +1,15 @@
+const setupCircle = () => {
+  return ` <div class="container" id="container">
+      <div class="circle"></div>
+
+      <p id="text"></p>
+
+      <div class="pointer-container">
+        <span class="pointer"> </span>
+      </div>
+
+      <div class="gradient-circle"></div>
+    </div>`
+}
+
+export default setupCircle
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
 /**imports  */
-import './style.css' // ! this import is required for css file to work
+import './style.css' // ! The imported styles.css  required for css to work
 
 /** DOM elements */
 const container = document.getElementById('container') as HTMLDivElement
 const text = document.getElementById('text') as HTMLParagraphElement
 
-/** Timer variables: to chenage the time, it must be changed in the JS and CSS for example 7500 milliseconds in the javascript is 7s in the css file*/
-const totalTIme = 7500
-const breatheTime = (totalTIme / 5) * 2
+/** Timer variables: to change the time, it must be changed in the JS and CSS for example 7500 milliseconds in the javascript is 7s in the css file*/
+const totalTIme = 8500
 const holdTime = totalTIme / 5
+const breatheTime = holdTime * 2
 
 /** Animate the breath animation  */
 function breathAnimation() {
-  text.innerText = 'Breathe In!'
-  container.className = 'container grow'
+  text.innerText = 'Breath In!'
+  container.className = 'container grow' // ! change the class name to grow
 
   setTimeout(() => {
     text.innerText = 'Hold'
-
     setTimeout(() => {
-      text.innerText = 'Breathe Out!'
-      container.className = 'container shrink'
+      text.innerText = 'Breath Out'
+      container.className = 'container shrink' // ! change the class name to shrink
     }, holdTime)
   }, breatheTime)
 }
+breathAnimation()
 
-/** Call the breath animation function every 7.5 seconds */
+/**Set an interval for the animation to repeat every 7.5 seconds    */
 setInterval(breathAnimation, totalTIme)
